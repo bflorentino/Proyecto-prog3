9bdbf21a36b62a50122228fa6702e75427a95499
MiltonDuro <miltongp26@gmail.com>
2022-03-07 15:32:28 -0400

    Implementación de JWT y inicio de secion

--- Backend/Proyecto-Prog3/Capa de servicios/obj/Release/net5.0/Capa de servicios.AssemblyInfo.cs
+++ Backend/Proyecto-Prog3/Capa de servicios/obj/Release/net5.0/Capa de servicios.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Capa de datos")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Capa de servicios")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Capa de datos")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Capa de datos")]
+[assembly: System.Reflection.AssemblyProductAttribute("Capa de servicios")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Capa de servicios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
