3ab5bb321ce29698695606d1d0ab03edba3a1a58
johannytavarez <scarlet.tavarezg@gmail.com>
2022-04-01 17:12:53 -0400

    Historial de ventas

---
+++ Backend/Proyecto-Prog3/Capa de servicios/Modelos/FacturaViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capa_de_servicios.Modelos
+{
+    public class FacturaViewModel
+    {
+        public string CodigoFactura { get; set; }
+        public DateTime? Fecha { get; set; }
+        public decimal Monto { get; set; }
+    }
+}
---
+++ Backend/Proyecto-Prog3/Capa de servicios/Modelos/ItemsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capa_de_servicios.Modelos
+{
+    public class ItemsViewModel
+    {
+        //public string CodigoDetalle { get; set; }
+        //public string CodigoFactura { get; set; }
+        public int Idlibro { get; set; }
+        public int Cantidad { get; set; }
+        public int Precio { get; set; }
+        public string NombreLibro { get; set; }
+    }
+}
